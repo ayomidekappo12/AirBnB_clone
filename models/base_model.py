0bed569339a28f7e64b5e04da7dca957ca034d56
ayomidekappo <ayomidekappo12@gmail.com>
2023-05-13 11:30:02 +0100

    updated AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """ BaseModel Module """
 import uuid
 from datetime import datetime as dt
+import models
 
 
 class BaseModel:
@@ -19,10 +20,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = dt.now()
             self.updated_at = dt.now()
+            storage.new(self)
 
     def save(self):
         if self.created_at != dt.now():
             self.updated_at = dt.now()
+            storage.save()
 
     def to_dict(self):
         self.__dict__["__class__"] = self.__class__.__name__
