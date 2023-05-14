51cb4b5ee8b59e8b2c498e6aec2dc345e3f49f3c
Akinwale Akinjiola <akinjil@gmail.com>
2023-05-14 21:55:59 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ storage = engine.file_storage.FileStorage()
 class BaseModel:
     """BaseModel Class"""
     def __init__(self, *args, **kwargs):
+        """Initilize BaseModel"""
         if kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
@@ -28,15 +29,18 @@ class BaseModel:
             storage.new(self)
 
     def save(self):
+        """Update the updated_at"""
         if self.created_at != dt.now():
             self.updated_at = dt.now()
             storage.save()
 
     def to_dict(self):
+        """Generate Object dictionary"""
         self.__dict__["__class__"] = self.__class__.__name__
         self.__dict__["created_at"] = self.created_at.isoformat("T")
         self.__dict__["updated_at"] = self.updated_at.isoformat("T")
         return self.__dict__
 
     def __str__(self):
+        """Override the object string representation"""
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
