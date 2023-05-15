6739bb55b41fbc7350a87d828dd4ca0087c23bc5
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-05-15 08:43:46 +0000

    updated BaseModels

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 
-import json
-import os
-
-
-class FileStorage:
-=======
 """
 FileStorage that serializes and deserializes instances to a JSON file
 """
@@ -28,39 +21,10 @@ class FileStorage:
     """ String representing a simple data structure in JSON format.
         ex: '{ "12": { "numbers": [1, 2, 3], "name": "John" } }'
     """
->>>>>>> 5445d2765fc987d395eb6a949eb87aba334ef654
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-<<<<<<< HEAD
-        return FileStorage.__objects
-
-    def new(self, obj):
-        dict_key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[dict_key] = obj
-
-    def save(self):
-        sel_objects = {}
-        for k, v in FileStorage.__objects.items():
-            sel_objects[k] = v.dict()
-        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as f:
-            json.dump(sel_objects, f)
-
-    def reload(self):
-        try:
-	if not os.path.isfile(FileStorage.__file_path):
-                return
-            with open(FileStorage.__file_path, mode="r", encoding="utf-8") as f:
-                sel_objects = json.load(f)
-                for k, v in sel_objects.items():
-                    cls_name, obj_id = k.split(".")
-                    cls = eval(cls_name)
-                    obj = cls(**v)
-                    FileStorage.__objects[k] = obj
-        except FileNotFoundError:
-                pass
-=======
         """ returns the dictionary __objects """
         return self.__objects
 
@@ -86,4 +50,3 @@ class FileStorage:
                 json_obj = json.load(f)
             for key, val in json_obj.items():
                 self.__objects[key] = eval(val["__class__"])(**val)
->>>>>>> 5445d2765fc987d395eb6a949eb87aba334ef654
